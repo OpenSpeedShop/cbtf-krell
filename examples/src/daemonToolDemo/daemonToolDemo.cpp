584aa56b7b6fcb552f1b789f4832fb43884a4ba0
Don Maghrak <dpm@krellinst.org>
2011-07-14 12:22:39 -0500

    Move the daemonToolDemo to tools/examples/src/daemonToolDemo...

---
+++ examples/src/daemonToolDemo/daemonToolDemo.cpp
@@ -0,0 +1,202 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010,2011 Krell Institute. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <sstream>
+#include <boost/program_options.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <KrellInstitute/CBTF/BoostExts.hpp>
+#include <KrellInstitute/CBTF/Component.hpp>
+#include <KrellInstitute/CBTF/Type.hpp>
+#include <KrellInstitute/CBTF/ValueSink.hpp>
+#include <KrellInstitute/CBTF/ValueSource.hpp>
+#include <KrellInstitute/CBTF/XML.hpp>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+
+
+using namespace boost;
+using namespace KrellInstitute::CBTF;
+
+
+
+namespace std {
+
+    /**
+     * Redirect a std::map<std:string, Type> const iterator to an output stream.
+     * Defined in order to allow the Boost.Test macros to work properly.
+     *
+     * @param stream      Target output stream.
+     * @param iterator    Const iterator to redirect.
+     * @return            Target output stream.
+     */
+    std::ostream& operator<<(
+        std::ostream& stream,
+        const std::map<std::string, Type>::const_iterator& iterator
+        )
+    {
+        stream << "std::map<std::string, Type>::const_iterator";
+        return stream;
+    }
+
+    /**
+     * Redirect a std::set<Type> const iterator to an output stream. Defined
+     * in order to allow the Boost.Test macros to work properly.
+     *
+     * @param stream      Target output stream.
+     * @param iterator    Const iterator to redirect.
+     * @return            Target output stream.
+     */
+    std::ostream& operator<<(std::ostream& stream,
+                             const std::set<Type>::const_iterator& iterator)
+    {
+        stream << "std::set<Type>::const_iterator";
+        return stream;
+    }
+    
+} // namespace std
+
+
+/**
+ * Main function for the example PC sampling tool.
+ */
+
+class DaemonToolDemo
+{
+  public:
+
+  DaemonToolDemo()
+  {
+  }
+
+  void start(const std::string& topology, const std::string& cmd)
+  {
+    dm_thread = boost::thread(&DaemonToolDemo::run, this, topology, cmd);
+  }
+
+  void join()
+  {
+    dm_thread.join();
+  }
+
+  void run(const std::string& topology, const std::string& cmd)
+  {
+
+    registerXML(filesystem::path(XMLDIR) / "daemonToolDemo.xml");
+
+    std::set<Type> available_types = Component::getAvailableTypes();
+
+
+    Component::registerPlugin(
+        filesystem::path(LIBDIR) / "KrellInstitute/CBTF/BasicMRNetLaunchers");
+    
+    Component::Instance network = Component::instantiate(
+        Type("Daemon_Tool_Demo")
+        );
+
+    Component::Instance launcher = Component::instantiate(
+        Type("BasicMRNetLauncherUsingBackendCreate")
+        );
+
+    boost::shared_ptr<ValueSource<filesystem::path> > topology_file =
+        ValueSource<filesystem::path>::instantiate();
+    boost::shared_ptr<ValueSource<std::string> > input_value =
+        ValueSource<std::string>::instantiate();
+    boost::shared_ptr<ValueSink<std::vector<std::string> > > output_value =
+        ValueSink<std::vector<std::string> >::instantiate();
+
+    Component::Instance topology_file_component = 
+        reinterpret_pointer_cast<Component>(topology_file);
+    Component::Instance input_value_component =
+        boost::reinterpret_pointer_cast<Component>(input_value);
+    Component::Instance output_value_component =
+        boost::reinterpret_pointer_cast<Component>(output_value);
+
+    Component::connect(
+        topology_file_component, "value", launcher, "TopologyFile"
+        );
+
+    Component::connect(launcher, "Network", network, "Network");
+    Component::connect(input_value_component, "value", network, "in");
+    Component::connect(network, "out", output_value_component, "value");
+
+    // start mrnet network
+    *topology_file = topology;
+    // send the command to run on backend (on backend filter)
+    *input_value = cmd;
+
+    // display the results of the command...
+    std::vector<std::string> outval = *output_value;
+    std::vector<std::string>::const_iterator ci;
+    for(ci = outval.begin(); ci != outval.end(); ci++) {
+            std::cout << *ci << std::endl;
+    }
+
+  }
+
+  private:
+	boost::thread dm_thread;
+};
+
+int main(int argc, char** argv)
+{
+    std::string topology;
+    std::string cmd;
+
+    // create a default for topology file.
+    char const* home = getenv("HOME");
+    std::string default_topology(home);
+    default_topology += "/.cbtf/cbtf_topology";
+    std::string default_cmd = "ps -ef";
+
+    boost::program_options::options_description desc("pcsampDemo options");
+    desc.add_options()
+        ("help,h", "Produce this help message.")
+        ("topology",
+	    boost::program_options::value<std::string>(&topology)->default_value(default_topology),
+	    "Path name to a valid mrnet topology file. (i.e. from mrnet_topgen),")
+        ("cmd",
+	    boost::program_options::value<std::string>(&cmd)->default_value(default_cmd),
+	    "Command to execute on backend tool daemon. Must be in quotes if command has arguments.")
+        ;
+
+    boost::program_options::variables_map vm;
+
+    // handle any regular options
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+    boost::program_options::notify(vm);    
+
+    // handle any positional options
+    boost::program_options::positional_options_description p;
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
+				  options(desc).positional(p).run(), vm);
+    boost::program_options::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    DaemonToolDemo dtdemo;
+    dtdemo.start(topology,cmd);
+
+    dtdemo.join();
+}
---
+++ examples/src/daemonToolDemo/daemonToolDemoPlugin.cpp
@@ -0,0 +1,286 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010,2011 Krell Institute. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+/** @file Plugin used by unit tests for the CBTF MRNet library. */
+
+#include <boost/bind.hpp>
+#include <mrnet/MRNet.h>
+#include <typeinfo>
+
+#include <KrellInstitute/CBTF/Component.hpp>
+#include <KrellInstitute/CBTF/Type.hpp>
+#include <KrellInstitute/CBTF/Version.hpp>
+
+using namespace KrellInstitute::CBTF;
+
+
+
+/**
+ * Component that converts an std::string value into a MRNet packet.
+ */
+class __attribute__ ((visibility ("hidden"))) ConvertStringToPacket :
+    public Component
+{
+
+public:
+
+    /** Factory function for this component type. */
+    static Component::Instance factoryFunction()
+    {
+        return Component::Instance(
+            reinterpret_cast<Component*>(new ConvertStringToPacket())
+            );
+    }
+
+private:
+
+    /** Default constructor. */
+    ConvertStringToPacket() :
+        Component(Type(typeid(ConvertStringToPacket)), Version(0, 0, 1))
+    {
+        declareInput<std::string>(
+            "in1", boost::bind(&ConvertStringToPacket::inHandler1, this, _1)
+            );
+        declareOutput<MRN::PacketPtr>("out1");
+
+        declareInput<std::vector<std::string> >(
+            "in2", boost::bind(&ConvertStringToPacket::inHandler2, this, _1)
+            );
+        declareOutput<MRN::PacketPtr>("out2");
+    }
+
+    /** Handler for the "in" input.*/
+    void inHandler1(const std::string& in)
+    {
+        emitOutput<MRN::PacketPtr>(
+            "out1", MRN::PacketPtr(new MRN::Packet(0, 0, "%s", in.c_str()))
+            );
+    }
+    void inHandler2(const std::vector<std::string>& in)
+    {
+	char** arr = NULL;
+	arr = (char**) malloc( in.size() * sizeof(char*) );
+
+	std::vector< std::string >::const_iterator iter = in.begin();
+	for( unsigned u = 0; iter != in.end() ; iter++, u++ ) {
+
+	    arr[u] = strdup(iter->c_str());
+	}
+        emitOutput<MRN::PacketPtr>(
+            "out2", MRN::PacketPtr(new MRN::Packet(0, 0, "%as", arr, in.size() ))
+            );
+    }
+    
+}; // class ConvertStringToPacket
+
+KRELL_INSTITUTE_CBTF_REGISTER_FACTORY_FUNCTION(ConvertStringToPacket)
+
+
+
+/**
+ * Component that converts a MRNet packet into an string value.
+ */
+class __attribute__ ((visibility ("hidden"))) ConvertPacketToString :
+    public Component
+{
+
+public:
+
+    /** Factory function for this component type. */
+    static Component::Instance factoryFunction()
+    {
+        return Component::Instance(
+            reinterpret_cast<Component*>(new ConvertPacketToString())
+            );
+    }
+
+private:
+
+    /** Default constructor. */
+    ConvertPacketToString() :
+        Component(Type(typeid(ConvertPacketToString)), Version(0, 0, 1))
+    {
+        declareInput<MRN::PacketPtr>(
+            "in1", boost::bind(&ConvertPacketToString::inHandler1, this, _1)
+            );
+        declareOutput<std::string>("out1");
+
+        declareInput<MRN::PacketPtr>(
+            "in2", boost::bind(&ConvertPacketToString::inHandler2, this, _1)
+            );
+        declareOutput<std::vector<std::string> >("out2");
+    }
+
+    /** Handler for the "in" input.*/
+    void inHandler1(const MRN::PacketPtr& in)
+    {
+	char* val = NULL;
+        in->unpack("%s", &val);
+        emitOutput<std::string>("out1", std::string(val));
+    }
+    void inHandler2(const MRN::PacketPtr& in)
+    {
+        std::vector<std::string> out;
+	char** val = NULL;
+	unsigned len = 0;
+        in->unpack("%as", &val, &len);
+	for( unsigned u=0; u < len; u++ ) {
+	    out.push_back(std::string(val[u]));
+	}
+        emitOutput<std::vector<std::string> >("out2", out);
+    }
+    
+}; // class ConvertPacketToString
+
+KRELL_INSTITUTE_CBTF_REGISTER_FACTORY_FUNCTION(ConvertPacketToString)
+
+
+
+/**
+ * Component that calls the ps command.
+ */
+class __attribute__ ((visibility ("hidden"))) PopenCommand :
+    public Component
+{
+
+public:
+
+    /** Factory function for this component type. */
+    static Component::Instance factoryFunction()
+    {
+        return Component::Instance(
+            reinterpret_cast<Component*>(new PopenCommand())
+            );
+    }
+
+private:
+
+    /** Default constructor. */
+    PopenCommand() :
+        Component(Type(typeid(PopenCommand)), Version(0, 0, 1))
+    {
+        declareInput<std::string>(
+            "in", boost::bind(&PopenCommand::inHandler, this, _1)
+            );
+        declareOutput< std::vector<std::string> >("out");
+    }
+
+    /** Handler for the "in" input.*/
+    void inHandler(const std::string& in)
+    {
+	std::string command;
+
+	if (in == "" ) {
+	    command = "ps -ef";
+	} else {
+	    command = in;
+	}
+
+	FILE *fd = NULL;
+	char buffer[ BUFSIZ ];
+	memset(&buffer,0,sizeof(buffer));
+
+	std::vector<std::string> psout;
+	std::string line;
+
+	fd = popen( in.data(), "r" );
+
+	if( fd != NULL ) {
+            std::cout << std::endl;
+            while( fgets( buffer, sizeof( buffer ), fd ) ) {
+                std::string fline(buffer);
+		if (!fline.empty() && fline[fline.length()-1] == '\n') {
+		    fline.erase(fline.length()-1);
+		}
+		if (fline != "") {
+		    psout.push_back(fline);
+		}
+		line = fline;
+            }
+	    pclose( fd );
+            std::cout  << std::endl;
+
+            fd = NULL;
+
+        } else {
+            std::cerr << "Error opening " << in << std::endl;
+        }
+
+//DEBUG
+#if 0
+	std::vector<std::string>::const_iterator ci;
+	for(ci=psout.begin(); ci!=psout.end(); ci++) {
+	    std::cout << *ci << std::endl;
+	}
+#endif
+
+        emitOutput< std::vector<std::string> >("out", psout);
+    }
+    
+}; // class PopenCommand
+
+KRELL_INSTITUTE_CBTF_REGISTER_FACTORY_FUNCTION(PopenCommand)
+
+
+/**
+ */
+class __attribute__ ((visibility ("hidden"))) DisplayData :
+    public Component
+{
+
+public:
+
+    /** Factory function for this component type. */
+    static Component::Instance factoryFunction()
+    {
+        return Component::Instance(
+            reinterpret_cast<Component*>(new DisplayData())
+            );
+    }
+
+private:
+
+    /** Default constructor. */
+    DisplayData() :
+        Component(Type(typeid(DisplayData)), Version(0, 0, 1))
+    {
+        declareInput<std::string>(
+            "in1", boost::bind(&DisplayData::displayHandler1, this, _1)
+            );
+        declareOutput<std::string>("out1");
+
+        declareInput<std::vector<std::string> >(
+            "in2", boost::bind(&DisplayData::displayHandler2, this, _1)
+            );
+        declareOutput<std::vector<std::string> >("out2");
+    }
+
+    /** Handler for the "in" input.*/
+    void displayHandler1(const std::string& in)
+    {
+	std::cout << in << std::endl;
+    }
+
+    void displayHandler2(const std::vector<std::string>& in)
+    {
+	std::cout << "TODO for displayHandler2..." << std::endl;
+    }
+
+}; // class DisplayData
+
+KRELL_INSTITUTE_CBTF_REGISTER_FACTORY_FUNCTION(DisplayData)

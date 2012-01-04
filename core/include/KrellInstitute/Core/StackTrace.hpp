dfcead9ffd493889d3453a3166dd94074b1e86cb
Don Maghrak <dpm@krellinst.org>
2012-01-03 18:19:21 -0600

    Add a simple StackTrace class to core libs. Based on OSS class but without knowledge of linkedobject,function,statements (for now).

---
+++ core/include/KrellInstitute/Core/StackTrace.hpp
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012 The Krell Institute. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+/** @file
+ *
+ * Declaration of the StackTrace class.
+ *
+ */
+
+#ifndef _StacktraceData_
+#define _StacktraceData_
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "KrellInstitute/Core/Address.hpp"
+#include "KrellInstitute/Core/AddressBuffer.hpp"
+#include "KrellInstitute/Core/ThreadName.hpp"
+#include "KrellInstitute/Core/Time.hpp"
+
+#include <set>
+#include <vector>
+#include <utility>
+
+
+
+namespace KrellInstitute { namespace Core {
+
+    /**
+     * Stack trace.
+     *
+     * Ordered list of addresses representing the function call sequence at a
+     * particular moment in a thread's execution. The first (zero index) entry
+     * in the list is the address of the currently executing instruction. The
+     * last (highest index) entry is in the start routine (e.g. "_start") of
+     * the thread. Entries in between contain caller addresses representing
+     * the sequence of function calls that led to the current instruction.
+     *
+     * @sa    http://en.wikipedia.org/wiki/Stack_trace
+     *
+     * @ingroup CollectorAPI ToolAPI
+     */
+    class StackTrace :
+	public std::vector<Address>
+    {
+
+    public:
+
+	/** Constructor from thread and time. */
+	StackTrace(const ThreadName& thread, const Time& time) :
+	    std::vector<Address>(),
+	    dm_thread(thread),
+	    dm_time(time)
+	{
+	}
+
+	/** Read-only data member accessor function. */
+	Thread getThread() const
+	{
+	    return dm_thread;
+	}
+
+	/** Read-only data member accessor function. */
+	Time getTime() const
+	{
+	    return dm_time;
+	}
+
+
+    private:
+
+	/** Thread in which this stack trace was recorded. */
+	Thread dm_thread;
+	
+	/** Time at which this stack trace was recorded. */
+	Time dm_time;
+	
+    };
+    
+} }
+
+
+
+#endif

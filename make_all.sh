12734c1b6a006b947f9f5a412754541c8110eeb3
Jim Galarowicz <jeg@krellinst.org>
2012-05-31 10:24:04 -0700

    Add a short script that builds all the subdirectories.

---
+++ make_all.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+cd messages
+make clean; make; make install
+cd ..
+cd services
+make clean; make; make install
+cd ..
+cd core
+make clean; make; make install
+cd ..
+cd contrib
+make clean; make; make install
+cd ..
+cd examples
+make clean; make; make install
+cd ..
+cd test
+make clean; make; make install
+cd ..

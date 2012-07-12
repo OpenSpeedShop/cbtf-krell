faeeca76a15515edcfe66b368f5498ed497403ef
Don Maghrak <dpm@krellinst.org>
2012-07-11 23:58:09 -0500

    Add a general daemonTool to core.  This client is based off a discussion with Phil Romero at LANL on the need for a more generalized daemon tool. The intent here is to provide a client that can run a daemon tool specified entirely by an xml file and the component plugins registered by the xml file with no other implementation details of the tool coded in the daemonTool client itself.  This is the initial code and is not ready or built at this time. More to come...

---
+++ core/daemonTool/daemonTool.cpp
@@ -0,0 +1,266 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012 Krell Institute. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <KrellInstitute/CBTF/BoostExts.hpp>
+#include <KrellInstitute/CBTF/Component.hpp>
+#include <KrellInstitute/CBTF/Type.hpp>
+#include <KrellInstitute/CBTF/ValueSink.hpp>
+#include <KrellInstitute/CBTF/ValueSource.hpp>
+#include <KrellInstitute/CBTF/XML.hpp>
+#include <string>
+
+using namespace boost;
+using namespace KrellInstitute::CBTF;
+using namespace std;
+
+
+
+/**
+ * Class wrapping the main implementation of the example daemon tool.
+ */
+class DaemonTool
+{
+    
+public:
+    
+    DaemonTool()
+    {
+    }
+    
+    void start(const string& topology,
+               const string& tool,
+               const unsigned int& numBE)
+    {
+        dm_thread = thread(&DaemonTool::run, this, topology, tool, numBE);
+    }
+
+    void join()
+    {
+        dm_thread.join();
+    }
+
+
+    // The tool argument specifies an xml file where at minimum a Frontend
+    // and Backend network must be defined. (expand on this).
+    void run(const string& topology,
+             const string& tool,
+             const unsigned int& numBE)
+    {
+        //
+        // The tool argument is a string that represents a CBTF distributed
+        // component network tool specified in an xml file.  The xml must
+        // specify at a minumum a <Frontend> and <Backend> component network
+        // internally.  It is optional to specify <Filter> component networks
+        // for placement on nodes where commuication processes may exist in
+        // the topology (CP's are mrnet_commnodes where filter plugins may
+        // be loaded).
+        // In order to make use of the CBTF distrbuted (via MRNet) component
+        // network defined in the tool .xml passed by the tool argument we
+        // must first register that XML file with CBTF. The string defined
+        // by the tool argument does not include the xml name explicitly
+        // since we also use that name later below to identify the network.
+        // i.e. MyTool names the tool definition as MyTool.xml and the
+        // network instance as MyTool (which is encoded in the xml and
+        // referenced below where the network is instantiated).
+
+	std::string xmlfile(tool);
+	xmlfile += ".xml";
+	registerXML(filesystem::path(XMLDIR) / xmlfile);
+
+        //
+        // The plugin BasicMRNetLaunchers contains, you guessed it, two basic
+        // MRNet launchers - one that uses MRNet's backend create mode, and one
+        // that uses MRNet's backend attach mode. Since we want to utilize the
+        // distributed component network "tool" exclusive of the type
+        // of launcher employed, that network does not specify a launcher. We
+        // will create one explicitly in just a moment. That means, however,
+        // that "tool" doesn't list BasicMRNetLaunchers in its list
+        // of plugins. Thus we must register it explicitly here.
+        //
+
+        Component::registerPlugin(
+            filesystem::path(LIBDIR) / "KrellInstitute/CBTF/BasicMRNetLaunchers.so"
+            );
+
+        //
+        // Create an instance of the "tool" distributed component
+        // network. This constructs the component network that resides on the
+        // frontend, but does not yet attempt to construct the filter/backend
+        // component networks. This full network is not instantiated until a
+        // MRNet Network object is received on the Network input. This input
+        // is not defined in "tool".xml - it is automagically added
+        // by CBTF to ALL MRNet-based CBTF distributed component networks.
+        //
+        
+        Component::Instance network = Component::instantiate(
+            Type(tool)
+            );
+
+        //
+        // Create an instance of the basic MRNet launcher that uses the backend
+        // create mode. This component takes a MRNet topology file as input and
+        // has a Network output that is of the MRNet Network type.
+        // 
+
+        Component::Instance launcher = Component::instantiate(
+            Type("BasicMRNetLauncherUsingBackendCreate")
+            );
+
+        //
+        // Instruct CBTF to connect the Network output of the launcher
+        // component to the Network input of our "tool" instance.
+        // 
+
+        Component::connect(launcher, "Network", network, "Network");
+
+        //
+        // In order to pass values between "raw" C++ code and a CBTF component
+        // network, bridge objects are used. The CBTF ValueSource and ValueSink
+        // template classes function as these bridges. In this example we have
+        // two inputs to, and one output from, the "tool"+launcher
+        // combination:
+        //
+        //     topology_file: The name of the topology file describing the
+        //                    MRNet network to be constructed. This will be
+        //                    passed into the launcher.
+        //
+        //           command: The tool to be executed.
+        //
+        // Note that unlike most other CBTF components, the Value[Source|Sink]
+        // components are explictly instantiated via their instantiate() method.
+        // In order for CBTF to connect these bridge components into our network
+        // we must cast them to a Component::Instance as well.
+        //
+
+        shared_ptr<ValueSource<filesystem::path> > topology_file =
+            ValueSource<filesystem::path>::instantiate();
+
+
+        Component::Instance topology_file_component = 
+            reinterpret_pointer_cast<Component>(topology_file);
+        
+        //
+        // Connect the bridges to the appropriate inputs and output of the
+        // launcher and "tool" components.
+        //
+
+        Component::connect(
+            topology_file_component, "value", launcher, "TopologyFile"
+            );
+
+        //
+        // Now that all the components have been hooked up, we are finally
+        // ready to get things going. The BasicMRNetLauncherUsingBackendCreate
+        // launcher needs a single input - the path of the MRNet topology file.
+        // Once this value is passed in, the launcher calls MRNet to construct
+        // the MRNet network and then emits the Network object on its output,
+        // where it travels to the "tool" component and causes the
+        // complete CBTF distrubted (via MRNet) component network to be created.
+        // 
+
+        *topology_file = topology;
+
+    } // run
+    
+private:
+	thread dm_thread;
+};
+
+
+
+/**
+ * Main function for the example daemon tool.
+ */
+int main(int argc, char** argv)
+{
+    //
+    // Create a default topology file location that is used if one isn't
+    // specified as a command-line argument.
+    //
+
+    char const* home = getenv("HOME");
+    string default_topology(home);
+    default_topology += "/.cbtf/cbtf_topology";
+
+    //
+    // Parse the command-line arguments using Boost.Program_options.
+    //
+
+    unsigned int numBE;
+    string topology;
+    string tool;
+    
+    program_options::options_description desc("daemonTool options");
+
+    desc.add_options()
+        ("help,h", "Produce this help message.")
+
+        ("numBE",
+         program_options::value<unsigned int>(&numBE)->default_value(1),
+         "Number of expected mrnet backends. Default is 1, This should "
+         "match the number of nodes in your topology and job allocation.")
+        
+        ("topology",
+         program_options::value<string>(&topology)->
+         default_value(default_topology),
+         "Path name to a valid mrnet topology file. (i.e. from mrnet_topgen),")
+
+        ("tool",
+         program_options::value<string>(&cmd)->default_value(default_cmd),
+         "Tool specified in xml to execute.")
+        ;
+    
+    program_options::variables_map vm;
+
+    // Handle any regular options
+    program_options::store(
+        program_options::parse_command_line(argc, argv, desc), vm
+        );
+    program_options::notify(vm);    
+
+    // Handle any positional options
+    program_options::positional_options_description p;
+    program_options::store(
+        program_options::command_line_parser(argc, argv).
+        options(desc).positional(p).run(), vm
+        );
+    program_options::notify(vm);
+
+    if (vm.count("help"))
+    {
+        cout << desc << endl;
+        return 1;
+    }
+    
+    cout << "Running tool " << tool 
+         << "\nNumber of mrnet backends: "  << numBE
+         << endl;
+    
+    //
+    // Construct an instance of DaemonTool, initiate a separate thread
+    // to run the command, and wait for that thread to complete.
+    //
+
+    DaemonTool dt;
+    dt.start(topology, tool, numBE);
+    dt.join();
+}

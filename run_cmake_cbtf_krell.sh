37f9a9462a221ca69caefc4831c1ff123a7ae4af
Gregory Schultz <gregory.schultz@embarqmail.com>
2017-02-27 20:18:36 -0500

    cmake batch script change.

--- run_cmake_cbtf_krell.sh
+++ run_cmake_cbtf_krell.sh
@@ -9,11 +9,11 @@ export CXX=g++
 
 export KRELL_ROOT=/opt/STABLE/krellroot_v2.3.0
 export DYNINST_ROOT=/opt/STABLE/krellroot_v2.3.0
-export CBTF_ROOT=/opt/TEST/cbtf_v2.3.1
-export CBTF_KRELL_PREFIX=/opt/TEST/cbtf_v2.3.1
+export CBTF_ROOT=/opt/DEVEL3-TEST/cbtf_v2.3.1
+export CBTF_KRELL_PREFIX=/opt/DEVEL3-TEST/cbtf_v2.3.1
 export MRNET_ROOT=/opt/STABLE/krellroot_v2.3.0
 export XERCESC_ROOT=/opt/STABLE/krellroot_v2.3.0
-export BOOST_ROOT=/usr
+export BOOST_ROOT=/opt/boost-1.59.0
 
 cmake .. \
         -DCMAKE_BUILD_TYPE=None \

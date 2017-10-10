21388c4176fc7bcc0d7e959bbd11913936f39511
Gregory Schultz <gregory.schultz@embarqmail.com>
2017-10-09 23:55:23 -0600

    Power8 development tweaks.

--- run_cmake_cbtf_krell.sh
+++ run_cmake_cbtf_krell.sh
@@ -7,18 +7,18 @@ pushd build
 export CC=gcc
 export CXX=g++
 
-export KRELL_ROOT=/opt/STABLE/krellroot_v2.3.0
-export DYNINST_ROOT=/opt/STABLE/krellroot_v2.3.0
-export CBTF_ROOT=/opt/DEVEL3-TEST/cbtf_v2.3.1
-export CBTF_KRELL_PREFIX=/opt/DEVEL3-TEST/cbtf_v2.3.1
-export MRNET_ROOT=/opt/STABLE/krellroot_v2.3.0
-export XERCESC_ROOT=/opt/STABLE/krellroot_v2.3.0
-export BOOST_ROOT=/opt/boost-1.59.0
+export KRELL_ROOT=~/OSS/krellroot_v2.3.1
+export DYNINST_ROOT=~/OSS/krellroot_v2.3.1
+export CBTF_ROOT=~/OSS/cbtf_v2.3.1
+export CBTF_KRELL_PREFIX=~/OSS/cbtf_v2.3.1
+export MRNET_ROOT=~/OSS/krellroot_v2.3.1
+export XERCESC_ROOT=~/OSS/krellroot_v2.3.1
+export BOOST_ROOT=~/OSS/krellroot_v2.3.1
 
 cmake .. \
         -DCMAKE_BUILD_TYPE=None \
-        -DCMAKE_CXX_FLAGS="-g -O2 -m64 -mcmodel=medium" \
-        -DCMAKE_C_FLAGS="-g -O2 -m64 -mcmodel=medium" \
+        -DCMAKE_CXX_FLAGS="-g -O2" \
+        -DCMAKE_C_FLAGS="-g -O2" \
 	-DCMAKE_INSTALL_PREFIX=${CBTF_KRELL_PREFIX} \
 	-DCMAKE_PREFIX_PATH=${CBTF_ROOT} \
 	-DCBTF_DIR=${CBTF_ROOT} \
@@ -31,10 +31,10 @@ cmake .. \
 	-DXERCESC_DIR=${XERCESC_ROOT} \
         -DLIBDWARF_DIR=${KRELL_ROOT} \
         -DLIBELF_DIR=${KRELL_ROOT} \
-	-DOPENMPI_DIR=/opt/openmpi-2.0.1 \
+	-DOPENMPI_DIR=/opt/OPENMPI/2.1.1/xl64 \
 	-DCBTF_BOOST_ROOT=${BOOST_ROOT}
 
 make clean
-make
+make VERBOSE=1
 make install
 

88f08ddd55029f460225123318f198e1d4f83d26
Gregory Schultz <gregory.schultz@embarqmail.com>
2017-11-11 21:19:26 -0800

    Update cmake script.

--- run_cmake_cbtf_krell.sh
+++ run_cmake_cbtf_krell.sh
@@ -7,7 +7,7 @@ pushd build
 export CC=gcc
 export CXX=g++
 
-export KRELL_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
+export KRELL_ROOT=/u/glschult/OSS/krellroot_v2.3.1
 export DYNINST_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
 export MRNET_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
 export XERCESC_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
@@ -16,12 +16,15 @@ export BOOST_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
 export CBTF_ROOT=/u/glschult/OSS/osscbtf_v2.3.1
 export CBTF_KRELL_PREFIX=/u/glschult/OSS/osscbtf_v2.3.1
 export OPENMPI_INSTALL_ROOT=/nasa/openmpi/1.6.5/gcc
-export CUDA_DIR=/nasa/cuda/7.5
+#export CUDA_DIR=/nasa/cuda/7.5
+export CUDA_DIR=/nasa/cuda/sles12/8.0
+export ICU_PATH=/nasa/pkgsrc/sles12/2016Q4
+#export ICU_PATH=/nasa/pkgsrc/2016Q2
 
 cmake .. \
         -DCMAKE_BUILD_TYPE=None \
-        -DCMAKE_CXX_FLAGS="-g -O2 -m64 -mcmodel=medium" \
-        -DCMAKE_C_FLAGS="-g -O2 -m64 -mcmodel=medium" \
+        -DCMAKE_CXX_FLAGS="-g -O2" \
+        -DCMAKE_C_FLAGS="-g -O2" \
 	-DCMAKE_INSTALL_PREFIX=${CBTF_KRELL_PREFIX} \
 	-DCMAKE_PREFIX_PATH=${CBTF_ROOT} \
 	-DCBTF_DIR=${CBTF_ROOT} \
